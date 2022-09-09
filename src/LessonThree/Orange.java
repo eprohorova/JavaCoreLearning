6abd12a2b69807ff9d4a3fd5b14727131ed2df56
eprohorova <eprohorova505@gmail.com>
2022-09-09 19:10:01 +0200

    commit

---
+++ src/LessonThree/Orange.java
@@ -0,0 +1,12 @@
+package LessonThree;
+
+public class Orange extends Fruit{
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+}
