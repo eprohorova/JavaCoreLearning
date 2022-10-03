64dc75a19bef463c1b5a2bdbb9c9415b8cc6b6ff
Ekaterina Prohorova <eprohorova505@gmail.com>
2022-10-03 16:54:06 +0200

    commit

---
+++ src/LessonNine/Room.java
@@ -0,0 +1,30 @@
+package LessonNine;
+
+import java.util.Objects;
+
+public class Room {
+
+    import java.util.Objects;
+    private String name;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(name, room.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
